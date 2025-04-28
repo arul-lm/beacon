8300f2ed34c04a4342e52a58ecf6b0bab0fd69ad
arulselvan1234@gmail.com <arulselvan1234@gmail.com>
2025-04-28 21:55:22 +0000

    Example

--- ccl/xpu.go
+++ ccl/xpu.go
@@ -1,17 +1,92 @@
 package ccl
 
 import (
+	"fmt"
 	"gitlab.com/akita/akita/v3/sim"
+	"log"
 )
 
 type Xpu struct {
 	*sim.TickingComponent
-	ports []sim.Port
-	msgs []sim.Msg
+	ports     []sim.Port
+	msgs      []sim.Msg
+	sendBytes uint64
+	recvBytes uint64
+	recvMsgs  map[sim.Msg]bool
 }
 
-func NewXpu(engine sim.Engine, freq sim.Freq, name string, numPorts int) *Xpu{
-	xpu := &Xpu{}
-	xpu.TickingComponent = sim.NewTickingComponent(name, engine, freq, xpu)
-	return xpu
+func (x *Xpu) Ports() []sim.Port {
+	return x.ports
+}
+
+func (x *Xpu) AddMsg(msg sim.Msg) {
+	x.msgs = append(x.msgs, msg)
+}
+func (x *Xpu) MsgsInFlight() int {
+	return len(x.msgs)
+}
+
+func (x *Xpu) Tick(now sim.VTimeInSec) bool {
+	// name := x.TickingComponent.Name()
+	madeProgress := x.send(now)
+	// fmt.Println("XPU Tick:", name, len(x.msgs), len(x.recvMsgs), x.sendBytes, x.recvBytes, madeProgress)
+	recvRes := x.recv(now)
+	madeProgress = recvRes || madeProgress
+	// fmt.Println("XPU Tick:", name, len(x.msgs), len(x.recvMsgs), x.sendBytes, x.recvBytes, recvRes, madeProgress)
+	return madeProgress
+}
+
+func (x *Xpu) send(now sim.VTimeInSec) bool {
+	if len(x.msgs) == 0 {
+		return false
+	}
+	msg := x.msgs[0]
+	msg.Meta().SendTime = now
+	err := msg.Meta().Src.Send(msg)
+	if err == nil {
+		x.msgs = x.msgs[1:]
+		x.sendBytes += uint64(msg.Meta().TrafficBytes)
+		return true
+	}
+	return false
+}
+
+func (x *Xpu) recv(now sim.VTimeInSec) bool {
+	madeProgress := false
+	// name := x.TickingComponent.Name()
+	for _, port := range x.ports {
+		msg := port.Retrieve(now)
+		if msg != nil {
+			if msg.Meta().Dst != port {
+				panic("Msg delivered at the wrong destination")
+			}
+			if _, found := x.recvMsgs[msg]; found {
+				panic("Msg already received")
+			}
+			x.recvMsgs[msg] = true
+			x.recvBytes += uint64(msg.Meta().TrafficBytes)
+			madeProgress = true
+			// fmt.Println("Received msg", name, msg)
+		}
+	}
+	return madeProgress
+}
+
+func NewXpu(engine sim.Engine, freq sim.Freq, name string, numPorts int) *Xpu {
+	x := &Xpu{}
+	x.ports = make([]sim.Port, numPorts)
+	x.TickingComponent = sim.NewTickingComponent(name, engine, freq, x)
+	for i := range numPorts {
+		p := sim.NewLimitNumMsgPort(x, 1, fmt.Sprintf("%sPort", name))
+		x.ports[i] = p
+	}
+	x.recvMsgs = make(map[sim.Msg]bool)
+	return x
+}
+
+func (x *Xpu) ReportBW(now sim.VTimeInSec) {
+	log.Printf("xpu %s, send bandwidth %.2f GB/s, recv bandwidth %.2f GB/s",
+		x.Name(),
+		float64(x.sendBytes)/float64(now)/1e9,
+		float64(x.recvBytes)/float64(now)/1e9)
 }
--- cmd/ccl/allreduce.go
+++ cmd/ccl/allreduce.go
@@ -2,14 +2,72 @@ package main
 
 import (
 	"fmt"
+	"github.com/arul-lm/beacon/ccl"
 	tsim "github.com/arul-lm/triosim/networkmodel"
-	"github.com/beacon/ccl"
 	"gitlab.com/akita/akita/v3/sim"
+	"math/rand"
 )
 
+type trafficMsg struct {
+	sim.MsgMeta
+}
+
+func (m *trafficMsg) Meta() *sim.MsgMeta {
+	return &m.MsgMeta
+}
+
+func GenerateMsgs(xpus []*ccl.Xpu, n uint64) []sim.Msg {
+	msgs := make([]sim.Msg, n)
+	for i := uint64(0); i < n; i++ {
+		srcAgentID := rand.Intn(len(xpus))
+		srcAgent := xpus[srcAgentID]
+		srcPortID := rand.Intn(len(srcAgent.Ports()))
+		srcPort := srcAgent.Ports()[srcPortID]
+
+		dstAgentID := rand.Intn(len(xpus))
+		for dstAgentID == srcAgentID {
+			dstAgentID = rand.Intn(len(xpus))
+		}
+
+		dstAgent := xpus[dstAgentID]
+		dstPortID := rand.Intn(len(dstAgent.Ports()))
+		dstPort := dstAgent.Ports()[dstPortID]
+		msg := &trafficMsg{}
+		msg.Meta().ID = sim.GetIDGenerator().Generate()
+		msg.Src = srcPort
+		msg.Dst = dstPort
+		// msg.TrafficBytes = rand.Intn(4096)
+		msg.TrafficBytes = 64 * 1e9
+		srcAgent.AddMsg(msg)
+		msgs[i] = msg
+	}
+	for _, x := range xpus {
+		fmt.Println("x:", x.MsgsInFlight())
+	}
+	return msgs
+}
+
 func main() {
-	// tsim.NewOpticalNetworkModel(${1:es sim.EventScheduler}, ${2:tt sim.TimeTeller}, ${3:maxNumWaveGuidesPerNode int}, ${4:establishLatency sim.VTimeInSec})
 	engine := sim.NewSerialEngine()
-	xpu1 := ccl.NewXpu(engine, engine, "xpu1", 7)
-	fmt.Println("Beacon - Allreduce", xpu1)
+	freq := sim.Freq(1)
+	numXpus := 2
+	ontw := tsim.NewOpticalNetworkModel(engine, engine, 1, 20)
+	xpus := make([]*ccl.Xpu, numXpus)
+	for x := range numXpus {
+		xpu := ccl.NewXpu(engine, freq, fmt.Sprintf("GPU%d", x), 1)
+		ontw.PlugIn(xpu.Ports()[0], 1)
+		xpu.TickLater(0)
+		xpus[x] = xpu
+	}
+	ontw.InitHardwareNetwork("mesh", 1, numXpus)
+	ontw.InitLogicalNetwork("ring", 1, numXpus)
+	GenerateMsgs(xpus, 100)
+	err := engine.Run()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Sim Time:", engine.CurrentTime())
+	for _, x := range(xpus) {
+		x.ReportBW(engine.CurrentTime())
+	}
 }
